376d1bf010dde230e75e5a70b3e123eb30585668
Irrelevxnce <FOS35514@educastur.es>
2022-02-03 20:31:28 +0100

    Revert "Segunda Actualizacion"
    This reverts commit 459cd66bbc102fae68c74011a4b203e7f2ca796d.

--- Test2.java
+++ Test2.java
@@ -3,8 +3,6 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("Test actualización");
-		System.out.println("Actualización 2");
 
 	}
 
